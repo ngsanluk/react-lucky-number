5ed530d20725a6c349723f39c2e4f80c709ce541
Sunny NG <ngsanluk@gmail.com>
2021-10-26 12:11:11 +0800

    adds lucky number logics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component  {
+  constructor(props) {
+    super(props);
+    this.state = { luckyNumber: 0 };
+    this.play = this.play.bind(this);
+  }
+
+  play(e) {
+    console.log("play() function called");
+    var luckyNumber = Math.floor((Math.random() * 100) + 1);
+    console.log(luckyNumber);
+    this.setState({luckyNumber: luckyNumber});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h3>Lucky Number</h3>
+          <h1 className="LuckyNumber">
+          {this.state.luckyNumber==0 ? '?' : this.state.luckyNumber}
+          </h1>
+          <button onClick={this.play}>Play</button>
+        </header>
+
+      </div>
+    );
+  }
 }
 
 export default App;
